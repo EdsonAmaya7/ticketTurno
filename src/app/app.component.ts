0ecbc0aaae96e29b327a9ed0943016c41c785636
EdsonAmaya7 <edson_amaya99@live.com>
2022-10-07 12:32:11 -0500

    install ractive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  registerForm!: FormGroup;
+  registerForm !: FormGroup;
   title = 'ticketAngular';
   submitted = false;
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      nombre_completo: [null, [Validators.required]],
+      nombre_completo : [null, [Validators.required]],
       //     lastName :  [null,[Validators.required]],
       //     email :  [null,[Validators.required, Validators.email]],
       //     password :  [null,[Validators.required, Validators.minLength(6)]]
