4570d031d14a431402dcbc46bb5d4bd1816a8055
EdsonAmaya7 <edson_amaya99@live.com>
2022-10-06 21:25:12 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  registerForm!: FormGroup;
   title = 'ticketAngular';
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      nombre_completo: [null, [Validators.required]],
+      //     lastName :  [null,[Validators.required]],
+      //     email :  [null,[Validators.required, Validators.email]],
+      //     password :  [null,[Validators.required, Validators.minLength(6)]]
+    });
+  }
+  onSubmit() {
+    this.submitted = true;
+    //stop the process if the form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      alert('success');
+    }
+  }
 }
