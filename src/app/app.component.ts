8ebbc8ac496e83657006d1049870f8234662ff40
EdsonAmaya7 <edson_amaya99@live.com>
2022-10-09 21:17:37 -0500

    validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.normalize.css','./app.component.css',]
 })
 export class AppComponent {
   registerForm !: FormGroup;
@@ -14,10 +14,16 @@ export class AppComponent {
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
-      nombre_completo : [null, [Validators.required]],
-      //     lastName :  [null,[Validators.required]],
-      //     email :  [null,[Validators.required, Validators.email]],
-      //     password :  [null,[Validators.required, Validators.minLength(6)]]
+      nombre_completo : [null,[Validators.required, Validators.minLength(3)]],
+      curp : [null, [Validators.required, Validators.pattern("/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/")]],
+      nombre  : [null, [Validators.required]],
+      apellido_paterno  : [null, [Validators.required, Validators.minLength(3)]],
+      apellido_materno  : [null, [Validators.required, Validators.minLength(3)]],
+      telefono  : [null, [Validators.required]],
+      correo  : [null, [Validators.required]],
+      asunto :  [null, [Validators.required]],
+      municipio :  [null, [Validators.required]],
+      nivel :  [null, [Validators.required]],
     });
   }
   onSubmit() {
